caf18925b0f5e7db45b2ab012eafe460bcb0be23
vuongnhattin <tinvuongnhat@gmail.com>
2024-09-13 22:39:01 +0700

    init

---
+++ src/test/java/com/tin/springcoursera/SpringCourseraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tin.springcoursera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
